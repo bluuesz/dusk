2c72b3acfb059915fd2eeccbfa7477c143b67735
Wosley Vacemberg <wosleyv@gmail.com>
2022-07-13 21:40:43 -0300

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,65 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.build.json'],
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 0,
+    'import/prefer-default-export': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-unsafe-member-access': 0,
+    '@typescript-eslint/no-unsafe-call': 0,
+    '@typescript-eslint/no-unsafe-argument': 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'no-restricted-syntax': 0,
+    'no-underscore-dangle': 0,
+    '@typescript-eslint/no-misused-promises': 0,
+    '@typescript-eslint/interface-name-prefix': 0,
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.tsx', '.ts'] },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
---
+++ todo.md
@@ -0,0 +1,9 @@
+#### TODO
+
+- [ ] setup monorepo
+- [ ] create anchor program
+- [ ] design program model
+- [ ] make donate
+- [ ] listen donates from backend
+- [ ] create widget to show in stream
+- [ ] create a dashboard to do minimal configuration
